18bafb8f321d65a79916834efbb0a09e92b73f85
lkc2140 <123655927+lkc2140@users.noreply.github.com>
2023-12-16 21:27:34 -0500

    Speaker movement/amplitude adjustment

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
-const circle = document.getElementById('circle');
+const circle = document.getElementById('listener');
 const x_pos = document.getElementById('x_pos');
 const y_pos = document.getElementById('y_pos');
+const speaker1 = document.getElementById('speaker1')
+const speaker2 = document.getElementById('speaker2')
+const speaker1_pos = document.getElementById('speaker1_pos')
+const speaker2_pos = document.getElementById('speaker2_pos')
+var d1 = speaker1_pos.value
+var d2 = speaker2_pos.value
 
 const room_len = 10 // room length in meters
 const pix_per_m = 600 / room_len
-const top1 = 250
-const top2 = 350
-const A = 0.45
+const amp1 = document.getElementById('amp1')
+const amp2 = document.getElementById('amp2')
+var A1 = amp1.value / 100
+var A2 = amp2.value / 100
+
 
 function move_pos() {
     var x = x_pos.value;
     var y = y_pos.value;
+    console.log(y)
     circle.style.transform = `translate(${x - 300}px, ${y - 300}px)`;
 }
+
+function move_speaker_pos() {
+    d1 = speaker1_pos.value
+    d2 = speaker2_pos.value
+    speaker1.style.transform = `translate(${0}px, ${-(280-d1)}px)`;
+    speaker2.style.transform = `translate(${0}px, ${d2-320}px)`;
+}
+
+
 
 x_pos.addEventListener('input', move_pos);
 y_pos.addEventListener('input', move_pos);
 
+speaker1_pos.addEventListener('input', move_speaker_pos);
+speaker2_pos.addEventListener('input', move_speaker_pos);
+
+
 const playButton = document.querySelector('button');
-
-
-
 playButton.addEventListener('click', async function () {
 
     audioCtx = new AudioContext()
@@ -31,42 +50,44 @@ playButton.addEventListener('click', async function () {
     speaker1.connect(gainNode);
     gainNode.connect(audioCtx.destination);
 
-    // phi_0 is 0 because r1 = r2
-    gainNode.gain.value = 2 * A * Math.cos(0)
+    // phi_0 is 0 because r1=r2
+    gainNode.gain.value = A1**2 + A2**2 + 2 * (A1 * A2 * Math.cos(0))
 
     speaker1.type = 'sine'; 
-    speaker1.frequency.value = 440; 
+    speaker1.frequency.value = 439.74; 
     var lambda = 343 / speaker1.frequency.value 
-    console.log(lambda)
 
     function adjust_gain() {
         var x = x_pos.value / pix_per_m
-        console.log(x)
         var y = y_pos.value;
-        console.log(y)
-        var y1 = (y - top1) / pix_per_m
-        console.log(y1)
-        var y2 = (top2 - y) / pix_per_m
-        console.log(y2)
+        var y1 = (y - d1) / pix_per_m
+        var y2 = (d2 - y) / pix_per_m
         var r1 = Math.sqrt(x**2 + y1**2)
-        console.log(r1)
         var r2 = Math.sqrt(x**2 + y2**2)
-        console.log(r2)
         var d_r = Math.abs(r1 - r2)
-        console.log(d_r)
         var phi = 2 * Math.PI * d_r / lambda
-        console.log(phi)
-        A_eff = 2 * A * Math.cos(phi / 2)
+        A_eff = Math.sqrt( A1**2 + A2**2 + 2 * (A1 * A2 * Math.cos(phi)))
+        console.log("A1")
+        console.log(A1)
+        console.log("A2")
+        console.log(A2)
+        console.log("A_eff")
         console.log(A_eff)
 
         var gainValue = A_eff ** 2;
-        console.log(gainValue)
         gainNode.gain.exponentialRampToValueAtTime(gainValue, audioCtx.currentTime + 0.2)
+    }
 
+    function change_amp() {
+        A1 = amp1.value / 100
+        A2 = amp2.value / 100
+        adjust_gain()
     }
 
     x_pos.addEventListener('input', adjust_gain);
     y_pos.addEventListener('input', adjust_gain);
+    amp1.addEventListener('input', change_amp);
+    amp2.addEventListener('input', change_amp);
 
     speaker1.start();
 
